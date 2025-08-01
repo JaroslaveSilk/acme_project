ccaf8584b75a714d2035f414b4223ada724279de
JaroslaveSilk <nigensey@yandex.ru>
2025-08-01 17:41:47 +0300

    Update views.py

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -6,9 +6,8 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    # Создаём экземпляр класса формы.
-    form = BirthdayForm()
-    # Добавляем его в словарь контекста под ключом form:
+    form = BirthdayForm(request.GET or None)
+    if form.is_valid():
+        pass
     context = {'form': form}
-    # Указываем нужный шаблон и передаём в него словарь контекста.
     return render(request, 'birthday/birthday.html', context)
